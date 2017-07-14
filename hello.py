02105aac562974d5ecf36efc31ac5387fc6560c3
Jobith M Basheer <jobithmbasheer@gmail.com>
2017-07-14 11:55:27 +0530

    new-features

--- hello.py
+++ hello.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
+import keyword
 
 print("Hello, World!")
+
+# variables
+x = 10
+#x = input("Enter a number: ")
+
+print(x)
+
+# keyword list
+print(keyword.kwlist)
+
+if keyword.iskeyword("name"):
+    print("`name is a keyword!`")
+else:
+    print("`name is not a keyword!`")
