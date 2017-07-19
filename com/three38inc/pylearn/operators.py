e89f21fd76fce23536c97a92592518d71591a8f2
Jobith M Basheer <jobithmbasheer@gmail.com>
2017-07-19 09:29:17 +0530

    refactor-to-package

---
+++ com/three38inc/pylearn/operators.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# Arithmetic Operators
+#   Addition        +
+#   Subtraction     -
+#   Multiplication  *
+#   Division        /
+#   Modulus         %
+#   Exponential     **
+#   Floor Division  //
+
+
+# Relational Operators
+# Logical Operators
+# Assignment Operators
+# Membership Operators
+# Identity Operators
