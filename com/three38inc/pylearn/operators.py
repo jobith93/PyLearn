51affb5686cabf11b6db0ba232468a89b0c0e072
Jobith M Basheer <jobithmbasheer@gmail.com>
2017-07-19 11:35:57 +0530

    add-semantic-comments

--- com/three38inc/pylearn/operators.py
+++ com/three38inc/pylearn/operators.py
@@ -13,9 +13,54 @@
 #   Floor Division  //
 
 
+n1 = 20; n2 = 10
+print("\nArithmetic Operations")
+print(n1, "+", n2, "=", (n1 + n2))
+print(n1, "-", n2, "=", (n1 - n2))
+print(n1, "*", n2, "=", (n1 * n2))
+print(n1, "/", n2, "=", (n1 / n2))
+print(n1, "%", n2, "=", (n1 % n2))
+print(n1, "**", n2, "=", (n1 ** n2))
+print(n1, "//", n2, "=", (n1 // n2))
+
 
 # Relational Operators
+print("\nRelational Operations")
+print("50 >= 75 -> ", (50>=75))
+print("50 > 75  -> ", (50>75))
+print("50 == 75 -> ", (50==75))
+print("50 <= 75 -> ", (50<=75))
+print("50 < 75  -> ", (50<75))
+
+
 # Logical Operators
+#   or
+#   and
+#   negate
+
+print("\nRelational Operations")
+isPlay = True
+isKid = True
+if isKid and isPlay:
+    print("Kid is happy")
+
+
 # Assignment Operators
+#   =
+#   +=
+#   -=
+#   *=
+#   /=
+
+
 # Membership Operators
+print("\nMembership Operations")
+names = ["sonu", "joel", "jobith"]
+print(names)
+print("Type of names: ", type(names), "\n")
+
+print("`sonu` is part of the list: ", "sonu"in(names))
+print("`jijo` is part of the list: ", "jijo"in(names))
+print("`joel` is part of the list: ", "joel"in(names))
+
 # Identity Operators
