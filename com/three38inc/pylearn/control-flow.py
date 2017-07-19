1de3e0b56d69d31506800be93d0021dbaf65b98d
Jobith M Basheer <jobithmbasheer@gmail.com>
2017-07-19 12:51:34 +0530

    control-flow-statements

--- com/three38inc/pylearn/control-flow.py
+++ com/three38inc/pylearn/control-flow.py
@@ -4,3 +4,73 @@
 # @author: Jobith M Basheer [https://github.com/jobith93]
 
 # Control Flow Statements
+#
+#   Decision Making
+#       if
+#       if-else
+#       elif
+#
+#   Looping
+#       for
+#       while
+#       do while
+#       foreach
+#
+#   Control Statements
+#       break
+#       continue
+#       pass
+
+print("\nControl Flow Statements")
+discount = 25
+if discount > 20:
+    print("Special Discount:", discount)
+
+
+marks = 70
+if 100 > marks > 80:
+    print("Grade: Distinction")
+elif 80 > marks > 60:
+    print("Grade: First Class")
+elif 60 > marks > 40:
+    print("Grade: Second Class")
+elif 40 > marks > 0:
+    print("Grade: Fail")
+else:
+    print("Invalid Marks")
+
+
+print("\nNumbers:", end=" ")
+for num in range(1, 6):
+    print(num, end=", ")
+
+
+marks = [44, 78, 55, 21, 98]
+print("\nMarks:", end=" ")
+for mark in marks:
+    print(mark, end=", ")
+
+
+# continue
+print("\nNumbers:", end=" ")
+for num in range(1, 10):
+    print(num, end=", ")
+    if num == 5:
+        continue
+
+
+# break
+print("\nNumbers:", end=" ")
+for num in range(1, 10):
+    print(num, end=", ")
+    if num == 5:
+        break
+
+
+# pass
+print("\nNumbers:", end=" ")
+for num in range(1, 10):
+    print(num, end=", ")
+    if num == 5:
+        pass
+
